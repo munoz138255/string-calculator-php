f4d1d10e31ed899b90b5539b98cb575d28412b4b
munoz138255 <munoz.138255@e.unavarra.es>
2025-03-25 12:10:44 +0100

    Primera integración

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,5 +4,123 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
+    public function __construct()
+    {
+    }
+
     // TODO: String Calculator Kata
+    public function Add(string $numbers): int
+    {
+        if ($this->isEmpty($numbers)) {
+            return 0;
+        }
+
+        if ($this->isDelimeterDeclared($numbers[0])) {
+            return $this->delimeterCleanedArray($numbers);
+        }
+
+        $numbersArray = $this->cleanArray($numbers, ",");
+
+        if ($this->isOnlyOneNumber($numbersArray)) {
+            return (int)$numbersArray[0];
+        }
+
+        return $this->getSum($numbersArray);
+    }
+
+    /**
+     * @param string $numbers
+     * @return bool
+     */
+    public function isEmpty(string $numbers): bool
+    {
+        return empty($numbers);
+    }
+
+    /**
+     * @param array $numbersArray
+     * @return bool
+     */
+    public function isOnlyOneNumber(array $numbersArray): bool
+    {
+        return count($numbersArray) === 1;
+    }
+
+    /**
+     * @param array $numbersArray
+     * @return int|mixed
+     */
+    public function getSum(array $numbersArray): int
+    {
+        $sum = 0;
+        $negativeNumbers = [];
+
+        foreach ($numbersArray as $number) {
+            $currentNumber = (int)$number;
+
+            if ($currentNumber < 0) {
+                $negativeNumbers[] = $currentNumber; // Agregamos al array
+            }
+            if($currentNumber < 1000){
+                $sum += $currentNumber;
+            }
+        }
+
+        if (!empty($negativeNumbers)) {
+            throw new \InvalidArgumentException("negativos no soportados: " . implode(", ", $negativeNumbers));
+        }
+
+        return $sum;
+    }
+    /**
+     * @param string $numbers
+     * @param string $delimiter
+     * @return string[]
+     */
+    public function cleanArray(string $numbers, string $delimiter): array
+    {
+        // Reemplazamos los saltos de línea por el delimitador correcto
+        $numbers = str_replace("\n", $delimiter, $numbers);
+        // Ahora explotamos usando el delimitador proporcionado (puede ser coma o el delimitador especial)
+        $numbersArray = explode($delimiter, $numbers);
+        return $numbersArray;
+    }
+
+    /**
+     * @param string $input
+     * @return array
+     */
+    public function obtainDelimeter(string $input): array
+    {
+        // Dividimos la cadena de entrada en líneas
+        $lines = explode("\n", $input);
+
+        // Extraemos el delimitador de la primera línea, después de "//"
+        $delimiter = substr($lines[0], 2, 1);  // Extrae el delimitador de "//[delimitador]"
+        // El segundo valor será la línea con los números
+        $numbers = implode("\n", array_slice($lines, 1));
+
+        return [$delimiter, $numbers];
+    }
+
+    /**
+     * @param $numbers
+     * @return bool
+     */
+    public function isDelimeterDeclared($numbers): bool
+    {
+        return $numbers === "/";
+    }
+
+    /**
+     * @param string $numbers
+     * @return int|mixed
+     */
+    public function delimeterCleanedArray(string $numbers): mixed
+    {
+        list($delimiter, $numbers) = $this->obtainDelimeter($numbers);
+
+        $numbersArray = $this->cleanArray($numbers, $delimiter);
+        return $this->getSum($numbersArray);
+    }
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -9,5 +9,90 @@ use PHPUnit\Framework\TestCase;
 
 final class StringCalculatorTest extends TestCase
 {
+
+    private StringCalculator $stringCalculator;
     // TODO: String Calculator Kata Tests
+
+
+    protected function setUp(): void{
+        parent::setUp();
+
+        $this->stringCalculator = new StringCalculator();
+    }
+    /**
+     * @test
+     */
+    public function ifEntryNullOutputIsZero(): void{
+        $result = $this->stringCalculator->Add("");
+
+        $this->assertEquals(0, $result);
+    }
+    /**
+     * @test
+     */
+    public function ifInputOneStringNumberOutputThatIntNumber(): void{
+        $result = $this->stringCalculator->Add("1");
+
+        $this->assertEquals(1, $result);
+    }
+    /**
+     * @test
+     */
+    public function ifInputTwoStringNumberOutputItsSum(): void{
+        $result = $this->stringCalculator->Add("1,2");
+
+        $this->assertEquals(3, $result);
+    }
+    /**
+     * @test
+     */
+    public function givenMultipleNumbersReturnItsSum(): void{
+        $result = $this->stringCalculator->Add("1,2,3,4,5,6,7,8,9");
+
+        $this->assertEquals(45, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenLineBreakReturnItsSum(): void{
+        $result = $this->stringCalculator->Add("1\n2,3,4,5,6\n7,8,9");
+
+        $this->assertEquals(45, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenDelimeterReturnItsSum(): void{
+        $result = $this->stringCalculator->Add("//:\n1\n2:3:4:5\n6\n7:8:9");
+
+        $this->assertEquals(45, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function givenNegativeNumbersThrowException(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("negativos no soportados: -2, -3");
+
+        $this->stringCalculator->Add("1,-2,-3,4");
+    }
+
+    /**
+     * @test
+     */
+    public function givenNumberGreaterThan1000IgnoreThatNumber(): void
+    {
+        $result = $this->stringCalculator->Add("//:\n1\n2:3000:9");
+
+        $this->assertEquals(12, $result);
+    }
+
+
+
+
+
 }
